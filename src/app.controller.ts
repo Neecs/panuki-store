e4c9c5b880ffbe50882c6c4dbec39bcc20d10f37
Nicolas Gil <nikosgil12@gmail.com>
2026-08-25 20:00:14 -0500

    initial test deleted

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('/sexo')
-  goodBye(): string {
-    return this.appService.goodBye();
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
-  goodBye(): string {
-    return 'odio las putas bien perras';
-  }
 }
