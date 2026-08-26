ad885bd7fe2ffc251a5361f0d467fe9c1c13beaf
Nicolas Gil <nikosgil12@gmail.com>
2026-08-26 14:32:51 -0500

    flautas kinky endpoint deleted

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -11,7 +11,7 @@ export class ProductController {
     return this.productService.createProduct(productData);
   }
 
-  @Get('flautas')
+  @Get()
   getAllProducts() {
     return this.productService.getAllProducts();
   }
